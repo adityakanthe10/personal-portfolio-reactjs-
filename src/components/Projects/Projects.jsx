c7eae97e837113a89c07b6234fe86d4f917bacc6
Aditya Kanthe <adityakanthe10@gmail.com>
2024-08-20 17:45:57 +0530

    added live link of the projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -30,7 +30,7 @@ const Projects = () => {
                 className="w-full max-w-xl lg:w-3/4 ">
                 <h6 className="mb-4 font-bold flex justify-between">{project.title} 
                      {/* <a className=""  target="_blank" href={project.githublink}><FaGithub /></a> */}
-                     <a className=""  target="_blank" href={project.link}><LiaExternalLinkAltSolid /></a></h6>
+                     <a className=""  target="_blank" href={project.livelink}><LiaExternalLinkAltSolid /></a></h6>
                     <p className="mb-4 text-neutral-400">{project.description}</p>
                     {project.technologies.map((tech,index)=>(
                         <span key={index} className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-500 shadow-neon">{tech}</span>
